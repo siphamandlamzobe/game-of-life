e0c75e460c5c8e373fdaaa597d6ec3585d1ddf7e
Siphamandla Mzobe <Siphamandla.Mzobe@kcpivot.co.za>
2022-02-16 09:52:22 +0200

    Renamed variable dot to cellContent.

--- GameOfLlife/BooleanArrayService.cs
+++ GameOfLlife/BooleanArrayService.cs
@@ -10,14 +10,15 @@ namespace GameOfLife
     {
         public bool[,] ConvertToBooleanArray(string[] rows, int rowLength, int rowCount)
         {
-            var dot = '.';
+            string cellContent = ".";
+
             bool[,] firstGeneration = new bool[rowCount, rowLength];
 
             for (int i = 0; i < rows.Length; i++)
             {
                 for (int j = 0; j < rows[i].Length; j++)
                 {
-                    if (rows[i][j] == dot)
+                    if ((rows[i][j]).ToString() == cellContent)
                     {
                         firstGeneration[i, j] = false;
                     }
