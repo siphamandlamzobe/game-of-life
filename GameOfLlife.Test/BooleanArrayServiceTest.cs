b8f4e732895eb44af5bb2eef532e1967683fde1b
Siphamandla Mzobe <Siphamandla.Mzobe@kcpivot.co.za>
2022-02-16 09:41:38 +0200

    Added a convert to boolean test

---
+++ GameOfLlife.Test/BooleanArrayServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife.Test
+{
+    public class BooleanArrayServiceTest
+    {
+        BooleanArrayService _booleanArrayService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _booleanArrayService = new BooleanArrayService();
+        }
+
+        [Test]
+        public void Given_StringArray_When_ConvertingToBooleanArray_Should_ReturnBooleanArray()
+        {
+            var stringArray = new string[]{ "........", "....*...", "...**...", "........" };
+
+            var rowLength = 8;
+
+            var rowCount = 4;
+
+            var actual = _booleanArrayService.ConvertToBooleanArray(stringArray, rowLength, rowCount);
+
+            var expected = new bool[,] {
+                { false, false, false, false, false, false, false, false },
+                { false, false, false, false, true, false, false, false },
+                { false, false, false, true, true, false, false, false },
+                { false, false, false, false, false, false, false, false } };
+
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
