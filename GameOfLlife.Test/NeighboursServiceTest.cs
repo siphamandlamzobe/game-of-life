0f700a0a7920e98a280b11a52089cfbcc2860c8f
Siphamandla Mzobe <Siphamandla.Mzobe@kcpivot.co.za>
2022-02-16 09:06:09 +0200

    Added a new test case for finding neigbours for the element (1,4).

--- GameOfLlife.Test/NeighboursServiceTest.cs
+++ GameOfLlife.Test/NeighboursServiceTest.cs
@@ -57,5 +57,21 @@ namespace GameOfLife.Test
             var actual = _neighboursService.FindNeighbours(firstGeneration, row, col);
             Assert.AreEqual(expected, actual);
         }
+
+        [Test]
+        public void Given_FirstGenerationGridForElement14_When_FindingNeighbours_Should_ReturnBooleanArrayWithNeigbhours()
+        {
+            var firstGeneration = new bool[,] {
+                { false, false, false, false, false, false, false, false },
+                { false, false, false, false, true, false, false, false },
+                { false, false, false, true, true, false, false, false },
+                { false, false, false, false, false, false, false, false } };
+
+            var expected = new bool[] { false, false, false, true, true, false, false, false };
+            var row = 1;
+            var col = 4;
+            var actual = _neighboursService.FindNeighbours(firstGeneration, row, col);
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
