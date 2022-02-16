777a7ebffa652a887b0c0604a21f4ef4d77ea396
Siphamandla Mzobe <Siphamandla.Mzobe@kcpivot.co.za>
2022-02-16 09:44:04 +0200

    Added another test case when given a string array with four asteriks.

--- GameOfLlife.Test/BooleanArrayServiceTest.cs
+++ GameOfLlife.Test/BooleanArrayServiceTest.cs
@@ -34,6 +34,25 @@ namespace GameOfLife.Test
                 { false, false, false, true, true, false, false, false },
                 { false, false, false, false, false, false, false, false } };
 
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Given_StringArrayWithFourAsteriks_When_ConvertingToBooleanArray_Should_ReturnBooleanArray()
+        {
+            var stringArray = new string[] { ".......*", "....*...", "...**...", ".......*" };
+
+            var rowLength = 8;
+
+            var rowCount = 4;
+
+            var actual = _booleanArrayService.ConvertToBooleanArray(stringArray, rowLength, rowCount);
+
+            var expected = new bool[,] {
+                { false, false, false, false, false, false, false, true },
+                { false, false, false, false, true, false, false, false },
+                { false, false, false, true, true, false, false, false },
+                { false, false, false, false, false, false, false, true } };
 
             Assert.AreEqual(expected, actual);
         }
